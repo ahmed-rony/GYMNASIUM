d8331ad063e6042853dc3826a13e9921cf27aa29
unknown <ahmedrony2101@gmail.com>
2021-11-28 21:43:43 -0800

    till about

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+// navbar activation;
+$(document).ready(function(){
+
+  //   banner slider   ==============;
+  $('.banner-slider').slick({
+      arrows: false,
+      dots: true,
+  });
+
+  $('.venobox').venobox(); 
+ 
+
+})
