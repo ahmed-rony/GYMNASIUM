b33777a189c60c4a4851024a3cbb08262b5e452a
unknown <ahmedrony2101@gmail.com>
2021-12-01 08:03:20 -0800

    till brand

--- js/custom.js
+++ js/custom.js
@@ -19,6 +19,22 @@ $(document).ready(function(){
 
   // funfact counter Up;
   $('.counter').counterUp();
+
+  // brand slider    ==================;
+  $('.brand-slider').slick({
+    slidesToShow: 5,
+    centerMode: true,
+    centerPadding: '0px',
+    prevArrow: '<i class="fas fa-chevron-left slick-prev prev-arrow"></i>',
+    nextArrow: '<i class="fas fa-chevron-right slick-next next-arrow"></i>',
+  })
+
+  $('.test-main').slick({
+    slidesToShow: 2,
+    // centerMode: true,
+    // centerPadding: '0px',
+    
+  })
  
 
 })
