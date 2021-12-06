b8e14fa2b333929aa54f11b6709ae49d5347871d
unknown <ahmedrony2101@gmail.com>
2021-12-05 22:23:52 -0800

    gymnasium complete

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,42 @@
 // navbar activation;
 $(document).ready(function(){
 
+  // scroll position   ==============;
+  $(window).on('scroll',function(){
+    var scrolling = $(this).scrollTop();
+
+    // back to top appearance   ===========;
+    if(scrolling > 200){
+      $('.back-to-top').fadeIn();
+    }else{
+      $('.back-to-top').fadeOut();
+      
+    }
+    // navbar positioning    ==============;
+    if(scrolling > 50){
+      $('nav').addClass('nav-fix');
+    }else{
+      $('nav').removeClass('nav-fix');
+    }
+
+  })
+  
+  // back to top action   ===========;
+  $('.back-to-top').on('click',function(){
+    $('html,body').animate({
+      scrollTop: '0px',
+    }, 1500);
+  })
+
+  
+
+
+
   //   banner slider   ==============;
   $('.banner-slider').slick({
       arrows: false,
       dots: true,
+      // autoplay: true,
   });
   
   // gallery venobox   =============;
@@ -15,6 +47,7 @@ $(document).ready(function(){
     arrows: false,
     slidesToShow: 2,
     dots: true,
+    // autoplay: true,
   });
 
   // funfact counter Up;
@@ -23,18 +56,13 @@ $(document).ready(function(){
   // brand slider    ==================;
   $('.brand-slider').slick({
     slidesToShow: 5,
+    // autoplay: true,
     centerMode: true,
     centerPadding: '0px',
     prevArrow: '<i class="fas fa-chevron-left slick-prev prev-arrow"></i>',
     nextArrow: '<i class="fas fa-chevron-right slick-next next-arrow"></i>',
   })
 
-  $('.test-main').slick({
-    slidesToShow: 2,
-    // centerMode: true,
-    // centerPadding: '0px',
-    
-  })
  
 
 })
